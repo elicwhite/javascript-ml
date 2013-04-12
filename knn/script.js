942f7a3f7f8abf811e0fe1de6cc115757f1ce19d
TheSavior <github@eli-white.com>
2013-04-11 23:37:04 -0700

    Implemented d3 example

--- knn/script.js
+++ knn/script.js
@@ -1,5 +1,11 @@
 window.onload = function() {
     "use strict";
 
-    var chart = new Chart();
+    var ele = document.getElementById("chart");
+
+    var chart = new Chart(ele);
+
+    var json = [[5.5, 2.6, 'a'],[3.2, 1.1, 'b'],[4.6, 2.1, 'a']];
+
+    chart.load(json);
 };
