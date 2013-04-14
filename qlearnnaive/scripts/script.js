57fe13b446080f1edd94bf975afbabe0682a13e6
TheSavior <github@eli-white.com>
2013-04-14 02:33:21 -0700

    Displaying the q array after training.

--- qlearnnaive/scripts/script.js
+++ qlearnnaive/scripts/script.js
@@ -11,15 +11,19 @@ window.onload = function() {
         [-1, 0, -1, -1, 0, 100]
     ];
 
+    var toColor = d3.scale.linear().domain([0, 100]).range(["red", "green"]);
+
+    var container = document.getElementById("box");
+
     var q = [];
-    for (var i = 0; i < reward.length; i++) {
+    for (i = 0; i < reward.length; i++) {
         q[i] = [];
         for (var j = 0; j < reward[0].length; j++) {
             q[i][j] = 0;
         }
     }
 
-    var gamma = .8;
+    var gamma = 0.8;
 
     var episodes = 10;
 
@@ -27,8 +31,8 @@ window.onload = function() {
 
     for (var episode = 0; episode < episodes; episode++) {
         var state = rand(0, reward.length);
-        
-        while(state != goal) {
+
+        while (state != goal) {
             // Action is the number of the state to go to
             var action = getRandAction(state);
 
@@ -39,10 +43,32 @@ window.onload = function() {
         }
     }
 
+    for (var i = 0; i < reward.length; i++) {
+        var row = document.createElement("div");
+
+        for (var j = 0; j < reward[0].length; j++) {
+            var qValue = q[i][j];
+
+            var cell = document.createElement("div");
+            cell.className = "cell";
+            cell.style.backgroundColor = toColor(qValue);
+
+            if (qValue != 0) {
+                cell.innerText = qValue;
+            }
+            row.appendChild(cell);
+        }
+
+        container.appendChild(row);
+    }
+
+
+
     document.getElementById("q").innerText = JSON.stringify(q);
 
 
     // Returns a random number between min inclusive and max exclusive.
+
     function rand(min, max) {
         return Math.round(Math.random() * (max - 1 - min) + min);
     }
@@ -50,7 +76,7 @@ window.onload = function() {
     function getRandAction(state) {
         var actions = [];
         for (var i = 0; i < reward[state].length; i++) {
-            if (reward[state][i] != -1){
+            if (reward[state][i] != -1) {
                 actions.push(i);
             }
         }
@@ -60,7 +86,7 @@ window.onload = function() {
 
     function maxQForState(state) {
         var max = q[state][0];
-        
+
         for (var i = 1; i < q[state].length; i++) {
             max = Math.max(max, q[state][i]);
         }
